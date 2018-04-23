b4ce678a86b1be8e88c48b2df14db7731b43aef5
unknown <amendoza@lagaceta.com.ar>
2018-04-23 04:49:15 -0300

    Version optimizada con evento de nuevos comentarios y carga de los mismos.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.get('/', (req, res) => { //routes
 })
 app.get('/nota/:notaId', (req, res) => {
     res.render('nota')
-    //var notaId = req.params.notaId
 })
 
 //Listen on port 3000
@@ -21,25 +20,19 @@ axios.defaults.baseURL = 'http://localhost:3000/demo_data/';
 
 const io = require("socket.io")(server) //socket.io instantiation
 
-io.on('connection', (socket) => { //listen on every connection
+io.on('connection', socket => { //listen on every connection
 
     var notaId = socket.handshake.query['notaId']
 
-    //console.log('New connection - ' + 'Socket ID: ' + socket.id)
     console.log('New connection - ' + 'Socket ID: ' + socket.id + ' - Nota ID: ' + notaId)
-    //console.log(socket)
 
-    // once a client has connected, we expect to get a ping from them saying what room they want to join
-    socket.on('nota', (notaId) => {
+    // Define room
+    socket.on('nota', notaId => {
         socket.join(notaId)
     })
 
     //listen on share
-    socket.on('share', (data) => {
-        //socket.notaId = notaId
-        //share = Math.floor(Math.random() * 100)
-        //share = JSON.parse(apiurl + 'share.json?notaId=' + notaId)
-
+    socket.on('share', data => {
         axios
             .get('share.json', {
                 params: {
@@ -47,14 +40,9 @@ io.on('connection', (socket) => { //listen on every connection
                 }
             })
             .then(response => {
-                //notaId = response.data.notaId
-                shareCount = response.data.count
+                io.sockets.to(notaId).emit('shareCountUpdate', {count: response.data.count})
 
-                //io.sockets.to(socket.id).emit('shareCountUpdate', {shareCount: shareCount})
-                io.sockets.to(notaId).emit('shareCountUpdate', {shareCount: shareCount})
-                //io.sockets.emit('shareCountUpdate', {shareCount: shareCount})
-
-                console.log('Socket ID: ' + socket.id + ' - Nota ID: ' + notaId + ' - ShareCount: ' + shareCount)
+                console.log('Socket ID: ' + socket.id + ' - Nota ID: ' + notaId + ' - Share Count: ' + response.data.count)
             })
             .catch(error => {
                 console.log(error)
@@ -62,32 +50,74 @@ io.on('connection', (socket) => { //listen on every connection
     })
 
     //listen on Comments Typing
-    socket.on('commentsAlert', (data) => {
+    /*socket.on('commentsAlert', data => {
         commentsAlert(data.type)
-    })
+    })*/
 
-    function commentsAlert(type) {
-        let message
+    //listen on Comments New Request
+    /*socket.on('commentsNewRequest', data => {
+        commentsAlert(data.type)
+    })*/
 
+    //Timer for comments New Request
+    setTimeout( () => {
+        commentsNewRequest()
+    }
+    , 5000)
+
+    function commentsNewRequest() {
+        //message = 'Hay ' + '5' + ' comentarios nuevos...'
+        //io.sockets.to(notaId).emit('commentsAlert', {type: type, message: message})
+
+        axios
+            .get('commentsNewRequest.json', {
+                params: {
+                    notaId: notaId
+                }
+            })
+            .then(response => {
+                io.sockets.to(notaId).emit('commentsNewRequest', {count: response.data.count})
+
+                console.log('Socket ID: ' + socket.id + ' - Nota ID: ' + notaId + ' - Comments New Count: ' + response.data.count)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    /*function commentsAlert(type) {
         switch (type) {
             case 'commentTyping':
                 message = socket.id + ' esta escribiendo un comentario...'
-                socket.broadcast.to(notaId).emit('commentsAlert', {message: message})
+                socket.broadcast.to(notaId).emit('commentsAlert', {type: type, message: message})
 
                 console.log('Socket ID Typing: ' + socket.id)
             break;
-            case 'commentsNewCount':
-                message = 'Hay ' + '5' + ' comentarios nuevos...'
-                io.sockets.to(notaId).emit('commentsAlert', {message: message})
-
-                console.log('New comments for display: ' + '5');
+            case 'commentsNewRequest':
+                commentsNewRequest(type)
             break;
             default:
-                console.log('Sorry, no type defined.');
+                console.log('Sorry, no type defined.')
         }
-    }
+    }*/
 
+    //listen on Comments Update
+    socket.on('commentsUpdate', data => {
+        axios
+            .get('comments.json', {
+                params: {
+                    notaId: notaId
+                }
+            })
+            .then(response => {
+                io.sockets.to(socket.id).emit('commentsNewLoad', {comments: response.data.comments})
 
+                console.log('Socket ID: ' + socket.id + ' - Nota ID: ' + notaId + ' - Comments New Load')
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    })
 
 
 
@@ -102,16 +132,16 @@ io.on('connection', (socket) => { //listen on every connection
     //default username
     socket.username = "Anonymous"
     //listen on change_username
-    socket.on('change_username', (data) => {
+    socket.on('change_username', data => {
         socket.username = data.username
     })
     //listen on new_message
-    socket.on('new_message', (data) => {
+    socket.on('new_message', data => {
         //broadcast the new message
         io.sockets.emit('new_message', {message: data.message, username: socket.username});
     })
     //listen on typing
-    socket.on('typing', (data) => {
+    socket.on('typing', data => {
         socket.broadcast.emit('typing', {username: socket.username})
     })
 })
--- public/LiveUpdate.js
+++ public/LiveUpdate.js
@@ -17,39 +17,47 @@ document.addEventListener('DOMContentLoaded', () => {
         //socket.emit('share', {notaId : notaId})
         socket.emit('share')
     })
-
     //Listen on shareCountUpdate
-    socket.on('shareCountUpdate', (data) => {
-        let shareCount = document.querySelector('#shareCount')
-        shareCount.textContent = 'Share Count: ' + data.shareCount
+    socket.on('shareCountUpdate', data => {
+        document.querySelector('#shareCount').textContent = 'Compartida: ' + data.count + ' veces.'
     })
 
-    //Emit typing
-    var commentsAlert = document.querySelector('#commentsAlert')
-    var commentsBox = document.querySelector('#commentsBox')
-    var commentsTextarea = document.querySelector('#commentsTextarea')
+    //comments
+    var commentsNewRequest = document.querySelector('#commentsNewRequest')
 
-    commentsTextarea.addEventListener('keypress', () => {
+    //Listen on commentsNewRequest
+    socket.on('commentsNewRequest', data => {
+        document.querySelector('#commentsNewRequestTxt').innerHTML =  'Hay ' + data.count + ' comentarios nuevos...'
+        commentsNewRequest.style.display = 'block'
+
+        console.logdata
+    })
+    //Emit a commentUpdate
+    document.querySelector('#commentsUpdate').addEventListener('click', () => {
+        socket.emit('commentsUpdate')
+        commentsNewRequest.style.display = 'none'
+    })
+    //Listen on commentsNewLoad
+    socket.on('commentsNewLoad', data => {
+        let comments = data.comments
+        let commentsHTML = ''
+
+        comments.forEach(comment => {
+            if (comment.level == 2) {
+                commentsHTML += '<p style="padding: 0 0 0 30px;">'
+            } else {
+                commentsHTML += '<p>'
+            }
+            commentsHTML += '<strong>' + comment.username + '</strong> - #' + comment.id + ' - Hace ' + comment.datetime + ' hs<br>' + comment.text + '<br>Like: ' + comment.like + ' - Unlike: ' + comment.unlike + '</p>'
+        })
+        document.querySelector('#commentsBox').insertAdjacentHTML('afterbegin', commentsHTML)
+    })
+
+    /*document.querySelector('#commentsTextarea').addEventListener('keypress', () => {
         socket.emit('commentsAlert', {type: 'commentTyping'})
-    })
-    //Listen on commentsTyping
-    socket.on('commentsAlert', (data) => {
+    })*/
+    //Listen on commentsAlert
+    /*socket.on('commentsAlert', data => {
         commentsAlert.innerHTML = data.message
-    })
-
-
-
-
-    //function commentsBoxUpdate(comments) {
-        //let commentsBox = document.querySelector('#comments');
-        //let commentsNew = ``
-
-        //comments.forEach(function (comments) {
-
-        //})
-
-        //commentsBox.insertAdjacentHTML('afterbegin', commentsNew)
-    //}
-
-
+    })*/
 });
